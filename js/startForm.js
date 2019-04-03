c5902ae75a3722b1e3b4d741d4c2d8722e30deda
Mark Nolledo <mark.nolledo@gmail.com>
2019-04-03 17:25:27 +0800

    Added and modified functions and styles

--- js/startForm.js
+++ js/startForm.js
@@ -1,4 +1,28 @@
 // Start form builder when form is ready
 $(document).ready(function() {
-  $(document.getElementById('build-wrap')).formBuilder();
+  //$(document.getElementById('build-wrap')).formBuilder();   // Basic form with no functions
+
+  // Form Builder with Form Render
+  jQuery(function($) {
+    var $fbEditor = $(document.getElementById('build-wrap')),
+      $formContainer = $(document.getElementById('build-render')),
+      $formHeaders = $(document.getElementById('build-render-headers')),
+      fbOptions = {
+        onSave: function() {
+          $fbEditor.toggle();
+          $formContainer.toggle();
+          $formHeaders.toggle();
+          $('form', $formContainer).formRender({
+            formData: formBuilder.formData
+          });
+        }
+      },
+      formBuilder = $fbEditor.formBuilder(fbOptions);
+
+    $('.edit-form', $formContainer).click(function() {
+      $fbEditor.toggle();
+      $formContainer.toggle();
+      $formHeaders.toggle();
+    });
+  });
 });
