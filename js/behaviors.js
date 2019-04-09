aa4481be391225066d8767d1739acc5224f717e8
Mark Nolledo <mark.nolledo@gmail.com>
2019-04-10 00:44:45 +0800

    Added a Register section to the extension

---
+++ js/behaviors.js
@@ -0,0 +1,9 @@
+var submitButton = document.getElementById('submit--reg-form');
+var registryForm = document.getElementById('register-form');
+
+// submitButton.addEventListener('click', demoScript);
+// registryForm.addEventListener('submit', redirectMe);
+
+function demoScript() {
+  alert('Hello World!');
+}
