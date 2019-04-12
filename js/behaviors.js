d0f85fef123d4f85284304d93fb5e676425d9ca5
Mark Nolledo <mark.nolledo@gmail.com>
2019-04-12 14:57:44 +0800

    Added Options page

--- js/behaviors.js
+++ js/behaviors.js
@@ -1,5 +1,6 @@
 var submitButton = document.getElementById('submit--reg-form');
 var registryForm = document.getElementById('register-form');
+var commentArea = document.querySelector('#comment');
 
 // submitButton.addEventListener('click', demoScript);
 // registryForm.addEventListener('submit', redirectMe);
@@ -7,3 +8,37 @@ var registryForm = document.getElementById('register-form');
 function demoScript() {
   alert('Hello World!');
 }
+
+// Open the options page
+document.querySelector('#link--options-page').addEventListener('click', function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  }
+  else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
+// Listen to the Lexicon Analysis form for submission
+// then POST the data using AJAX and retrieve a JSON response.
+$(function() {
+  $('#form-analyze-single').submit(function() {
+      $.ajax({
+          type: 'POST',
+          url: 'analyze/mode=lexicon',
+          data: {comment: commentArea.value},
+          error: function()
+          {
+             alert("Request Failed");
+          },
+          success: function(response)
+          {
+             //alert('Request Sent');
+             console.log(commentArea.value);
+             console.log(response.result);
+             $("#result").html(response.result);
+          } // this was missing
+      });
+      return false;
+  })
+});
