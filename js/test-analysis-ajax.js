4fc52482f636d0609c771d9240eb964a7f9c4bff
Mark Nolledo <mark.nolledo@gmail.com>
2019-04-16 15:08:00 +0800

    Updated options page, behaviors, and styling

--- js/test-analysis-ajax.js
+++ js/test-analysis-ajax.js
@@ -7,6 +7,15 @@ var commentAreaClassify = document.querySelector('#classify-comment')
 // Listen to the Lexicon Analysis form for submission
 // then POST the data using AJAX and retrieve a JSON response.
 $(document).ready(function() {
+  // Listener for an AJAX request begin/stop
+  $(document).ajaxStart(function() {
+    $('#toast--parent-container').fadeToggle(300);
+  });
+
+  $(document).ajaxStop(function() {
+    $('#toast--parent-container').fadeToggle(300);
+  });
+
   // Listener for the lexicon form
   $('#form-analyze-single').submit(function() {
       $.ajax({
@@ -31,24 +40,24 @@ $(document).ready(function() {
 
   // Listener for the classifier form
   $('#form-classify-single').submit(function() {
-      $.ajax({
-          type: 'POST',
-          url: 'http://marknolledo.pythonanywhere.com/sibyl/analyze/mode=lexicon',
-          data: {comment: commentAreaClassify.value},
-          error: function()
-          {
-             alert("Request Failed");
-             //alert(commentArea.value);
-          },
-          success: function(response)
-          {
-             //alert('Request Sent');
-             console.log('Text received: ' + commentArea.value);
-             console.log('Server response: ' + response.result);
-             $("#classify-result").html(response.result);
-          } // this was missing
-      });
-      return false;
+    $.ajax({
+        type: 'POST',
+        url: 'http://marknolledo.pythonanywhere.com/sibyl/analyze/mode=classifier',
+        data: {comment: commentAreaClassify.value},
+        error: function()
+        {
+           alert("Request Failed");
+           //alert(commentArea.value);
+        },
+        success: function(response)
+        {
+           //alert('Request Sent');
+           console.log('Text received: ' + commentArea.value);
+           console.log('Server response: ' + response.result);
+           $("#classify-result").html(response.result);
+        } // this was missing
+    });
+    return false;
   });
 
   //alert('Script reached.');
